7e17506f7701386412ec4b0634ffbae43bc11aa2
pawelstasiewicz <pst357357@gmail.com>
2024-02-11 12:01:08 +0100

    Add animation to Popup

--- src/components/atoms/YourProjectsComponent.jsx
+++ src/components/atoms/YourProjectsComponent.jsx
@@ -1,15 +1,18 @@
-function YourProjectsComponent({clickAddProject}) {
+function YourProjectsComponent({clickAddProject, taskElement}) {
 
   function handleClick(){
     clickAddProject(false)
   }
 
+
   return (
+    
     <div className="flex flex-col mt-20">
+      {console.log(taskElement)}
         <h1 className="text-white ml-10 font-sans uppercase font-semibold text-2xl">Your Projects</h1>
         <button className="bg-zinc-800 text-slate-500 w-40 ml-10 my-14 text-xl py-3 rounded-lg" onClick={handleClick}>+ Add Project</button>
         <ul className="flex flex-col">
-            <li className="text-slate-500 ml-14 text-xl">Learning React</li>
+            <li className="text-slate-500 ml-14 text-xl">1.{taskElement}</li>
         </ul>
     </div>
   )
--- src/components/molecules/SidebarComponent.jsx
+++ src/components/molecules/SidebarComponent.jsx
@@ -1,9 +1,9 @@
 import YourProjectsComponent from "../atoms/YourProjectsComponent"
 
-function SidebarComponent({clickAddProject}) {
+function SidebarComponent({clickAddProject, taskElement}) {
   return (
     <div className="bg-black h-full w-1/4 rounded-tr-3xl">
-      <YourProjectsComponent clickAddProject={clickAddProject}/>
+      <YourProjectsComponent clickAddProject={clickAddProject} taskElement={taskElement}/>
     </div>
   )
 }
